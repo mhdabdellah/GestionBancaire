665527c7a2229c8249bc049f5e86d043841a851b
abdellahi <mhdabdellahi0@gmail.com>
2022-12-09 11:48:03 +0100

    first commit

---
+++ src/main/java/RIM/Banque/GestionBancaire/GestionBancaireApplication.java
@@ -0,0 +1,13 @@
+package RIM.Banque.GestionBancaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBancaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBancaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RIM/Banque/GestionBancaire/GestionBancaireApplicationTests.java
@@ -0,0 +1,13 @@
+package RIM.Banque.GestionBancaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBancaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
